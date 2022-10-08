ee8a894c905138008145682ef313ef4a84e1069c
Bulat <bulatzinnurov99@gmail.com>
2022-10-08 15:43:35 +0300

    yandex

--- python/python.py
+++ python/python.py
@@ -1,1 +1,26 @@
-print("hello")
+DATABASE = {
+    'Серёга': 'Омск',
+    'Соня': 'Москва',
+    'Миша': 'Москва',
+    'Дима': 'Челябинск',
+    'Алина': 'Красноярск',
+    'Егор': 'Пермь',
+    'Коля': 'Красноярск'
+}
+
+def process_anfisa(query):
+    if query == 'Сколько у меня друзей?':
+        count = len(DATABASE)
+        return 'У тебя ' + str(count) + ' друзей.'
+    elif query == 'Кто все мои друзья?':  
+        friends_list = ''
+        for names in DATABASE.keys():
+            friends_list += names + ' '
+        return("Твои друзья: " + friends_list)  
+    else:
+        return '<неизвестный запрос>'
+
+# Не изменяйте следующий код
+print('Привет, я Анфиса!')
+print(process_anfisa('Сколько у меня друзей?'))
+print(process_anfisa('Кто все мои друзья?'))
